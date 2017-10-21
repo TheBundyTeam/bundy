83bc7d6dcfe49477101ad1febd2afae494bbaff9
Jordan711 <jordan.feng@hotmail.com>
2017-10-21 19:49:29 -0400

    Added functions for adding data
    User can add expenses and income to database

--- index.js
+++ index.js
@@ -2,16 +2,27 @@
 //var userName = getElementById("email");
 //var pass = getElementById("password");
 
-
+//User adds new income source
+var incomeName = getElementById("");
+//New or updated income amount
+var incomeAmount = getElementById("");
 
 // User adds new expense name or could be existing
 var expenseName = getElementById("");
-// New budget amount
+// New or updated budget amount
 var expenseAmount = getElementById("");
 
-function logIn(){
-	var fireData = firebase.database().ref();
+// Initialize the database
+var fireData = firebase.database().ref();
+
+function AddExpense(){
 	//Access te child nodes and set to value
 	fireData.child(expenseName).set(expenseName);
 	fireData.child(expenseName).child("value").set(expenseAmount);
+}
+
+function AddIncome(){
+	//Access te child nodes and set to value
+	fireData.child(incomeName).set(incomeName);
+	fireData.child(incomeName).child("value").set(incomeAmount);
 }
