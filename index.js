8f909b3e8749e00bd962b1ccf249fb31c9afd3dc
Jordan711 <jordan.feng@hotmail.com>
2017-10-21 21:23:55 -0400

    Dunno if this works tbh
    Attempted to implement the retrieval of data from the database.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Might delete when we get the authentication working
-//var userName = getElementById("email");
-//var pass = getElementById("password");
+var userName = getElementById("");
+var pass = getElementById("");
 
 //User adds new income source
 var incomeName = getElementById("");
@@ -12,17 +12,57 @@ var expenseName = getElementById("");
 // New or updated budget amount
 var expenseAmount = getElementById("");
 
-// Initialize the database
-var fireData = firebase.database().ref();
+/*
+function NewAccount(){
+	const auth = firebase.auth();
+	const account = auth.createUserWithEmailAndPassword(username.value, pass.value).catch(function(error){
+		alert(error.message);
+	});
+}
+
+function LogIn(){
+	const auth = firebase.auth();
+	const logged = auth.signInWithEmailAndPassword(userName.value, pass.value).catch(function(error){
+		alert(error.message);
+	});
+}
+
+function LogOut(){
+	firebase.auth.signOut().catch(function(error){
+		alert(error.message);
+	});
+}
+*/
 
 function AddExpense(){
-	//Access te child nodes and set to value
-	fireData.child(expenseName).set(expenseName);
-	fireData.child(expenseName).child("value").set(expenseAmount);
+	//Initialize the database
+	var fireData = firebase.database().ref();
+	//Access the child nodes and set to value
+	//For testing purposes, only one expense can be added and changed
+	fireData.child("expenses").set("expenses");
+	fireData.child("expenses").child("value").set(expenseAmount.value);
 }
 
 function AddIncome(){
-	//Access te child nodes and set to value
-	fireData.child(incomeName).set(incomeName);
-	fireData.child(incomeName).child("value").set(incomeAmount);
+	// Initialize the database
+	var fireData = firebase.database().ref();
+	//Access the child nodes and set to value
+	fireData.child("income").set("income");
+	fireData.child("income").child("value").set(incomeAmount.value);
+}
+
+function GetAllExpenses(){
+	var database = firebase.database().ref();
+	database.child("expenses").on('child_added', function(data, prev){
+		var info = data.val();
+		alert(info.value);
+	});
+}
+
+function GetAllIncome(){
+	var database = firebase.database().ref();
+	database.child("income").on('child_added', function(data, prev){
+		var info = data.val();
+		alert(info.value);
+	});
 }
