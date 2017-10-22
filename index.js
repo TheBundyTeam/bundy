e080b01e79913fd4edf9b716db63aabad3f98126
Jordan711 <jordan.feng@hotmail.com>
2017-10-22 03:29:57 -0400

    replaced user.uid with firebase.auth().currentUser.uid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function ModifyExpense(){
 	var total_expenses = "expense"; //document.getElementById("").val();
 	var expenseAmount = 666; //document.getElementById("").val();
-	firebase.database().ref('users/' + user.uid).set({
+	firebase.database().ref(firebase.auth().currentUser.uid).set({
 		total_expenses : expenseAmount
 	});
 }
@@ -12,24 +12,23 @@ function ModifyIncome(){
 	var total_budget = "income"; //document.getElementById("").val();
 	//New or updated income amount
 	var budget = 555; //document.getElementById("").val();
-	firebase.database().ref('users/' + user.uid).set({
+	firebase.database().ref(firebase.auth().currentUser.uid).set({
 		total_budget : budget
 	});
 }
 
 function GetExpenses(){
-	return firebase.database().ref('users/' + user.uid).on('value', GetData, Errors);
+	return firebase.database().ref(firebase.auth().currentUser.uid).on('value', GetData, Errors);
 }
 
 function GetData(data){
-	console.log(data.val());
 	return data.val();
 }
 
 function Errors(err){
-	console.log(err);
+	alert(err);
 }
 
 function GetIncome(){
-	return firebase.database().ref('users/' + user.uid).on('value', GetData, Errors);
+	return firebase.database().ref(firebase.auth().currentUser.uid).on('value', GetData, Errors);
 }
