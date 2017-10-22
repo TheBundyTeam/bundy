079639e2decfb0bad4bdf5d6afbb63bd442206d1
Jeffrey L <jeffclub51@yahoo.ca>
2017-10-21 20:16:23 -0400

    Added firebase authentication code

--- snippets.js
+++ snippets.js
@@ -1,5 +1,22 @@
 /* jshint node: true */
 var hasher = require('password-hash');
+var firebase = require('firebase');
+
+var authentication = firebase.auth();
+
+// creates user with email and password
+authentication.createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // handle errors here
+    var errorCode = error.code;
+    var errorMessage = error.message;
+});
+
+// sign in existing user
+authentication.signInWithEmailAndPassword(email, password).catch(function(error) {
+    // handle errors here
+    var errorCode = error.code;
+    var errorMessage = error.message;
+});
 
 function writeUserData(userId, email, password) {
     var database = firebase.database();
